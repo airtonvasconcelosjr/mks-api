cf31698f89778530353d6cd8d37a73a6c30827e3
airtonvasconcelosjr <airton.vasconcelosjr@gmail.com>
2023-06-30 14:06:21 -0300

    Backend changes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
 import CartModal from './CartModal';
-import { FaShoppingCart } from 'react-icons/fa';
+import { FaShoppingCart, FaShoppingBag } from 'react-icons/fa';
 import './styles.css';
 
 const App = () => {
@@ -9,6 +9,11 @@ const App = () => {
   const [loading, setLoading] = useState(true);
   const [cartItems, setCartItems] = useState([]);
   const [isCartOpen, setCartOpen] = useState(false);
+  const [itemCount, setItemCount] = useState(0);
+
+  const handleItemCountChange = (count) => {
+    setItemCount(count);
+  };
 
   useEffect(() => {
     const fetchData = async () => {
@@ -35,18 +40,16 @@ const App = () => {
 
   const handleAddToCart = (product) => {
     const existingItem = cartItems.find((item) => item.id === product.id);
-  
+
     if (existingItem) {
-      const updatedCartItems = cartItems.map((item) =>
-        item.id === product.id ? { ...item, quantity: item.quantity + 1 } : item
-      );
-      setCartItems(updatedCartItems);
-    } else {
-      const updatedCartItems = [...cartItems, { ...product, quantity: 1 }];
-      setCartItems(updatedCartItems);
+      return;
     }
+
+    const updatedCartItems = [...cartItems, { ...product, quantity: 1 }];
+    setCartItems(updatedCartItems);
+    handleItemCountChange(itemCount + 1);
   };
-  
+
   const handleRemoveFromCart = (itemId) => {
     const updatedCartItems = cartItems.filter((item) => item.id !== itemId);
     setCartItems(updatedCartItems);
@@ -109,7 +112,8 @@ const App = () => {
       <div>
         <h1 className="header">Header</h1>
         <button className="cartButton" onClick={() => handleCartOpen()}>
-          {(!loading && products.length > 0) && <FaShoppingCart />}
+          {!loading && products.length > 0 && <FaShoppingCart />}
+          <span className="itemCountNumber">{itemCount}</span>
         </button>
         <Routes>
           <Route
@@ -119,21 +123,27 @@ const App = () => {
                 {loading ? (
                   <div>{renderShimmers()}</div>
                 ) : products && products.length > 0 ? (
-                  products.map((product) => (
-                    <div key={product.id} className="product-card">
-                      <img src={product.photo} alt={product.name} className="product-image" />
-                      <h4 className="product-price">{product.price}</h4>
-                      <h2 className="product-name">{product.name}</h2>
-                      <p className="product-description">{product.description}</p>
-                      <button
-                        className="Comprar"
-                        aria-hidden="true"
-                        onClick={() => handleAddToCart(product)}
-                      >
-                        COMPRAR
-                      </button>
-                    </div>
-                  ))
+                  products.map((product, index) => {
+                    const isProductInCart = cartItems.some((item) => item.id === product.id);
+
+                    return (
+                      <div key={product.id} className="product-card">
+                        <img src={product.photo} alt={product.name} className="product-image" />
+                        <h4 className="product-price">{product.price}</h4>
+                        <h2 className="product-name">{product.name}</h2>
+                        <p className="product-description">{product.description}</p>
+                        <button
+                          className="Comprar"
+                          aria-hidden="true"
+                          onClick={() => handleAddToCart(product)}
+                          disabled={isProductInCart}
+                        >
+                          <FaShoppingBag />
+                          {isProductInCart ? 'Inserido no carrinho' : 'COMPRAR'}
+                        </button>
+                      </div>
+                    );
+                  })
                 ) : (
                   <p>No products available.</p>
                 )}
@@ -141,14 +151,18 @@ const App = () => {
             }
           />
         </Routes>
+
         {isCartOpen && (
-          <CartModal
-            cartItems={cartItems}
-            onClose={handleCartClose}
-            onRemoveFromCart={handleRemoveFromCart}
-            onIncreaseQuantity={handleIncreaseQuantity}
-            onDecreaseQuantity={handleDecreaseQuantity}
-          />
+          <div>
+            <CartModal
+              cartItems={cartItems}
+              onClose={handleCartClose}
+              onRemoveFromCart={handleRemoveFromCart}
+              onIncreaseQuantity={handleIncreaseQuantity}
+              onDecreaseQuantity={handleDecreaseQuantity}
+              onItemCountChange={handleItemCountChange}
+            />
+          </div>
         )}
         <h1 className="footer">Footer</h1>
       </div>
--- src/CartModal.js
+++ src/CartModal.js
@@ -1,12 +1,18 @@
 import React, { useState, useEffect } from 'react';
 
-const CartModal = ({ cartItems, onClose, onRemoveFromCart, onIncreaseQuantity, onDecreaseQuantity }) => {
+const CartModal = ({ cartItems, onClose, onRemoveFromCart, onItemCountChange }) => {
   const [itemQuantities, setItemQuantities] = useState({});
   const [totalPrice, setTotalPrice] = useState(0);
+  const [itemCount, setItemCount] = useState(0);
 
   useEffect(() => {
     calculateTotalPrice();
+    countItems();
   }, [cartItems, itemQuantities]);
+
+  useEffect(() => {
+    onItemCountChange(itemCount); 
+  }, [itemCount, onItemCountChange]);
 
   const handleRemoveItem = (itemId) => {
     onRemoveFromCart(itemId);
@@ -14,13 +20,13 @@ const CartModal = ({ cartItems, onClose, onRemoveFromCart, onIncreaseQuantity, o
     setItemQuantities(remainingQuantities);
   };
 
-
   const handleQuantityChange = (itemId, newQuantity) => {
-    setItemQuantities((prevQuantities) => ({
-      ...prevQuantities,
-      [itemId]: newQuantity,
-    }));
-    calculateTotalPrice();
+    if (newQuantity >= 1) {
+      setItemQuantities((prevQuantities) => ({
+        ...prevQuantities,
+        [itemId]: newQuantity,
+      }));
+    }
   };
 
   const getTotalPrice = (item) => {
@@ -36,47 +42,67 @@ const CartModal = ({ cartItems, onClose, onRemoveFromCart, onIncreaseQuantity, o
     setTotalPrice(total);
   };
 
+  const countItems = () => {
+    let count = 0;
+    cartItems.forEach((item) => {
+      count += itemQuantities[item.id] || 1;
+    });
+    setItemCount(count);
+    onItemCountChange(count); // Atualiza a contagem de itens no componente pai
+  };
+
+  const handleIncreaseQuantity = (itemId) => {
+    setItemQuantities((prevQuantities) => ({
+      ...prevQuantities,
+      [itemId]: (prevQuantities[itemId] || 1) + 1,
+    }));
+  };
+
+  const handleDecreaseQuantity = (itemId) => {
+    setItemQuantities((prevQuantities) => ({
+      ...prevQuantities,
+      [itemId]: Math.max((prevQuantities[itemId] || 1) - 1, 1),
+    }));
+  };
+
   return (
     <div className="cart-modal">
+      <button className="btn-close" onClick={onClose}>X</button>
       <h2>Shopping Cart</h2>
-      <button onClick={onClose}>Close</button>
       {cartItems.length > 0 ? (
-        <div>
+        <div className="cart-content">
           <ul className="cart-items">
             {cartItems.map((item) => (
-              <li key={item.id} className="cart-item">
+              <h5 key={item.id} className="cart-item">
                 <div>
                   <h3>{item.name}</h3>
                   <h3>{item.price}</h3>
                   <div>
-                    <button
-                      onClick={() => onIncreaseQuantity(item.id)}
-                    >
-                      +
-                    </button>
+                    <h5>Qtd:</h5>
+                    <button onClick={() => handleDecreaseQuantity(item.id)}>-</button>
                     <input
-                      type="number"
+                      className="qtd"
+                      inputMode="numeric"
                       min="1"
                       value={itemQuantities[item.id] || 1}
                       onChange={(e) => handleQuantityChange(item.id, parseInt(e.target.value))}
                     />
-                    <button
-                      onClick={() => onDecreaseQuantity(item.id)}
-                    >
-                      -
-                    </button>
+                    <button onClick={() => handleIncreaseQuantity(item.id)}>+</button>
                   </div>
                   <p>Total Price: ${getTotalPrice(item)}</p>
                   <button onClick={() => handleRemoveItem(item.id)}>Remove</button>
                 </div>
-              </li>
+              </h5>
             ))}
           </ul>
-          <h3>Total Purchase Price: {totalPrice}</h3>
+          <div className="cart-footer">
+            <h3 className="total-price">Total Purchase Price: {totalPrice}</h3>
+          </div>
         </div>
       ) : (
         <p>No items in the cart.</p>
       )}
+      <p>Item count: {itemCount}</p>
     </div>
   );
 };
